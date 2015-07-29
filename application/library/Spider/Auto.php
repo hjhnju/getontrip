61635e8a26024659b3d6b31437dd31687327bccf
fanyingying <fanyingying@xingjiaodai.com>
2015-07-29 15:20:52 +0800

    add code

---
+++ application/library/Spider/Auto.php
@@ -0,0 +1,21 @@
+﻿<?php 
+set_time_limit( 60 * 15 );
+class Spider_Auto{
+    
+    /**
+     * Spider_Auto::getContent($url)
+     * @param string $url
+     * @param string ，返回正文内容
+     */
+    public static function getContent($url){
+        $content    = file_get_contents($url); 
+        $pattern    = '/charset=(.*?)\"/si';
+        preg_match($pattern,$content,$match);
+        $sourceCode = $match[1];
+        $obj     = new Base_Extract($content);
+        $text    = $obj->getPlainText();
+        return  iconv($sourceCode,"utf8",$text);
+    }
+}
+    
+		

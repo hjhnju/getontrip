bdae0df9810899f3ad36906cfc4268b048cb167c
fanyingying <fanyingying@xingjiaodai.com>
2015-07-30 10:16:53 +0800

    add code

--- application/library/Spider/Web/Auto.php
+++ application/library/Spider/Web/Auto.php
@@ -16,15 +16,19 @@ class Spider_Web_Auto extends Spider_Web_Base{
      * @see Spider_Base::getBody()
      */
     public function getBody(){
-        $content    = file_get_contents($this->url);
-        $pattern    = '/<meta.*?>/si';
+        if(stristr($this->url,"www.zhihu.com")){
+            $content    = file_get_contents("compress.zlib://".$this->url);
+        }else{
+            $content    = file_get_contents($this->url);
+        }        
         $obj        = new Base_Extract($content,$this->url);
         $text       = $obj->getPlainText();
-        $num = preg_match_all($pattern,$content,$match);
+        $num = preg_match_all('/<meta.*?>/si',$content,$match);        
         for( $i = 0; $i < $num; $i++ ){
             if(false !== stristr($match[0][$i],"charset")){
                 preg_match('/charset=\"?(.*?)(\"|\s|\/|>)/si',$content,$match);
                 $sourceCode = trim($match[1]);
+                
                 return mb_convert_encoding($text,"utf8",$sourceCode);
             }
         }

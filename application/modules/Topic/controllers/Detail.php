bab5c9d44d5f95a07c74fa31aa05781339b3c21a
fanyingying <fanyingying@xingjiaodai.com>
2015-08-10 12:38:09 +0800

    topic detail

--- application/modules/Topic/controllers/Detail.php
+++ application/modules/Topic/controllers/Detail.php
@@ -1,39 +1,44 @@
 <?php
 /**
- * 话题详情页
- * @author huwei
- *
+ * 话题详情页面
+ * @author fyy
  */
-class DetailController extends Base_Controller_Api {
+class DetailController extends Base_Controller_Page {
     
-    const PAGESIZE = 6;
-    
+     
+   
     public function init() {
         $this->setNeedLogin(false);
-        parent::init();        
+        parent::init();
     }
     
     /**
-     * 接口1：/topic/detail/
-     * 话题详情页接口
-     * @param integer page
-     * @param integer pageSize
-     * @param integer topicId，话题ID
-     * @param string deviceId，用户的设备ID（因为要统计UV）
-     * @return json
+     *  详情
      */
-    public function indexAction() {
-        $page       = isset($_POST['page'])?intval($_POST['page']):1;
-        $pageSize   = isset($_POST['pageSize'])?intval($_POST['page']):self::PAGESIZE;
-        $topicId    = isset($_POST['topicId'])?intval($_POST['topicId']):'';
-        $deviceId   = isset($_POST['deviceId'])?trim($_POST['deviceId']):'';
-        $deviceId = 1;
-        $topicId = 1;
-        if(empty($deviceId) || empty($topicId)){
-            return $this->ajaxError(Base_RetCode::PARAM_ERROR,Base_RetCode::getMsg(Base_RetCode::PARAM_ERROR));
-        }        
-        $logic      = new Topic_Logic_Topic();
-        $ret        = $logic->getTopicDetail($topicId,$deviceId,$page,$pageSize);
-        $this->ajax($ret);
-    }   
+    public function indexAction() {             
+       $postid = isset($_REQUEST['id'])? intval($_REQUEST['id']) : 0; 
+        
+       $postInfo = Topic_Api::getTopicById($postid);
+	   if(!empty($postInfo)){
+	       //处理状态值  
+	       $postInfo["statusName"] = Topic_Type_Status::getTypeName($postInfo["status"]);  
+	       $this->getView()->assign('post', $postInfo);  
+       } 
+
+       
+    }
+    
+
+    /**
+     *  
+    */
+    public function previewAction() {             
+       $postid = isset($_REQUEST['id'])? intval($_REQUEST['id']) : 0;  
+       $postInfo = Topic_Api::getTopicById($postid);
+	   if(!empty($postInfo)){
+	       //处理状态值  
+	       $postInfo["statusName"] = Topic_Type_Status::getTypeName($postInfo["status"]);  
+	       $this->getView()->assign('post', $postInfo);  
+       } 
+    }
 }

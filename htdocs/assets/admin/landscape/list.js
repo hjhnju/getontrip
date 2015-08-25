43c96ff9e8962d8f9fe5668345a9e75661b37594
fanyingying <fanyingying@xingjiaodai.com>
2015-08-25 11:45:29 +0800

    pic sever

--- htdocs/assets/admin/landscape/list.js
+++ htdocs/assets/admin/landscape/list.js
@@ -45,7 +45,7 @@ $(document).ready(function() {
         }, {
             "data": function(e) {
                 if(e.image){
-                   return '<a href="/pic/' + e.image + '" target="_blank"><img alt="" src="/pic/' + e.image.getNewImgByImg(80,22) + '"/></a>';
+                   return '<a href="/pic/' + e.image + '" target="_blank"><img alt="" src="/pic/' + e.image.getNewImgByImg(80,22,'f') + '"/></a>';
                 }
                  return "未上传";
             }

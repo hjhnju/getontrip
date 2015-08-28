a4265b4c3a6dd10ac8abf560d201549614f0369f
fanyingying <fanyingying@xingjiaodai.com>
2015-08-28 12:31:36 +0800

    topic detail

--- application/modules/Topic/controllers/Detail.php
+++ application/modules/Topic/controllers/Detail.php
@@ -16,25 +16,43 @@ class DetailController extends Base_Controller_Page {
      *  详情
      */
     public function indexAction() {             
-       $postid = isset($_REQUEST['id'])? intval($_REQUEST['id']) : 0;  
-       $postInfo = Topic_Api::getTopicById($postid);
-       if(!empty($postInfo)){
-           //处理状态值  
-           $postInfo["statusName"] = Topic_Type_Status::getTypeName($postInfo["status"]);  
-           
-           //处理来源
-           $sourceInfo = Source_Api::getSourceInfo($postInfo['from']);
-           $postInfo['from_name'] = $sourceInfo['name'];
+       $postid = isset($_REQUEST['id'])? intval($_REQUEST['id']) : 0;
+       $deviceId   = isset($_REQUEST['deviceId'])?trim($_REQUEST['deviceId']):''; 
+       $logic      = new Topic_Logic_Topic();
+       $postInfo    = $logic->getTopicDetail($postid,$deviceId); 
+       //$postInfo = Topic_Api::getTopicById($postid);
+       if(!isset($postInfo['id'])){
+          $this->getView()->assign('post', array()); 
+       }else{ 
+        
+           //处理来源 
+          /* $sourceInfo = Source_Api::getSourceInfo($postInfo['from']);
+           $postInfo['from_name'] = $sourceInfo['name'];*/
+
+           //处理背景图片
+           if(!empty($postInfo["image"])){
+             $imgParams = Base_Image::getImgParams($postInfo["image"]);
+             $postInfo["img_hash"] = $imgParams['img_hash'];
+             $postInfo["img_type"] = $imgParams['img_type'];
+           }
+
+           //处理正文图片
+           $content = $postInfo['content'];  
+           if($content != ""){
+              $spider = Spider_Factory::getInstance("Filterimg",$content,Spider_Type_Source::STRING);
+              $postInfo['content'] = $spider->getContentToDis(); 
+           }
 
            $this->getView()->assign('post', $postInfo); 
        } 
-
+       
        //判断是否来自移动端
        $isMobile = Base_Util_Mobile::isMobile();
        //判断是否来自app
-       $isApp = isset($_REQUEST['fromapp'])? (bool)$_REQUEST['fromapp'] : false; 
+       $isapp = isset($_REQUEST['isapp'])?$_REQUEST['isapp'] : 0; 
        $this->getView()->assign('isMobile', $isMobile); 
-       $this->getView()->assign('isApp', $isApp); 
+       $this->getView()->assign('isapp', $isapp); 
+       //$this->getView()->assign('isfromApp', $isapp); 
 
        
     }
@@ -77,8 +95,8 @@ class DetailController extends Base_Controller_Page {
        //判断是否来自移动端
        $isMobile = Base_Util_Mobile::isMobile();
        //判断是否来自app
-       $isApp = isset($_REQUEST['isApp'])? (bool)$_REQUEST['isApp'] : false; 
+       $isapp = isset($_REQUEST['isapp'])? $_REQUEST['isapp'] : 0; 
        $this->getView()->assign('isMobile', $isMobile); 
-       $this->getView()->assign('isApp', $isApp);   
+       $this->getView()->assign('isapp', $isapp);   
     }
 }

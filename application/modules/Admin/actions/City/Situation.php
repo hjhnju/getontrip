7e852271ba69cccca171cb424b76038ad53a897d
fanyingying <fanyingying@xingjiaodai.com>
2015-10-04 07:19:41 +0800

    fix conflict

---
+++ application/modules/Admin/actions/City/Situation.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 城市总览
+ * @author fanyy
+ *
+ */
+class SituationAction extends Yaf_Action_Abstract {
+    public function execute() { 
+    	//$statusTypeArray=Topic_Type_Status::$names;
+    	//$this->getView()->assign('statusTypeArray', $statusTypeArray);
+    }
+}

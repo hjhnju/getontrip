a811851749a3626f788619db5c3cf1baf510c417
fanyingying <fanyingying@xingjiaodai.com>
2015-10-08 17:47:09 +0800

    add code

--- application/models/Sight.php
+++ application/models/Sight.php
@@ -93,7 +93,7 @@ class SightModel extends PgBaseModel
                 $_addValues[] = $_value;
             }
         }
-        $logicUser = new User_Logic_Login();
+        $logicUser = new User_Logic_Third();
         $userId = $logicUser->checkLogin();
         if(!empty($userId)){
             $_addFields[] = 'create_user';

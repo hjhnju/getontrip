c66c44b0c8dbe0cd25101e3e94459f354e3a9e65
fanyingying <fanyingying@xingjiaodai.com>
2015-11-02 16:54:32 +0800

    add code

--- application/modules/Topic/controllers/Api.php
+++ application/modules/Topic/controllers/Api.php
@@ -17,15 +17,17 @@ class ApiController extends Base_Controller_Api {
      * 接口1：/api/topic
      * 话题详情页接口
      * @param integer topicId，话题ID
+     * @param integer sightId, 景点ID
      * @return json
      */
     public function indexAction() {
         $topicId    = isset($_REQUEST['topicId'])?intval($_REQUEST['topicId']):'';
+        $sightId    = isset($_REQUEST['sightId'])?intval($_REQUEST['sightId']):'';
         if(empty($topicId)){
             return $this->ajaxError(Base_RetCode::PARAM_ERROR,Base_RetCode::getMsg(Base_RetCode::PARAM_ERROR));
         }        
         $logic      = new Topic_Logic_Topic();
-        $ret        = $logic->getTopicDetail($topicId);
+        $ret        = $logic->getTopicDetail($topicId,$sightId);
         $content    = $ret['content'];
         if($content != ""){
             $spider  = Spider_Factory::getInstance("Filterimg",$content,Spider_Type_Source::STRING);

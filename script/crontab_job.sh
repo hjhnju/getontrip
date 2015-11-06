d01a44bbf3efa61b9c6cc3f6d70602aa9ea0d8e0
fanyingying <fanyingying@xingjiaodai.com>
2015-11-06 10:43:52 +0800

    add code

--- script/crontab_job.sh
+++ script/crontab_job.sh
@@ -2,6 +2,7 @@
 url=http://123.57.46.229:8983
 curl $url/solr/city/dataimport
 curl $url/solr/sight/dataimport
+curl $url/solr/content/dataimport
 curl $url/solr/topic/dataimport
 curl $url/solr/book/dataimport
 curl $url/solr/wiki/dataimport

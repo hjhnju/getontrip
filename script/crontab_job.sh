dd200db94536f70adc08accd6828bad15cb50376
fanyingying <fanyingying@xingjiaodai.com>
2015-11-06 11:03:14 +0800

    add code

--- script/crontab_job.sh
+++ script/crontab_job.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
-url=http://123.57.46.229:8983
-curl $url/solr/city/dataimport
-curl $url/solr/sight/dataimport
-curl $url/solr/content/dataimport
-curl $url/solr/topic/dataimport
-curl $url/solr/book/dataimport
-curl $url/solr/wiki/dataimport
-curl $url/solr/video/dataimport
+offline=(123.57.46.229)
+
+online=(123.57.67.165 123.57.39.40 182.92.101.94)
+
+entity=(city sight content topic book wiki video)
+
+if [ $1 = "online" ];then
+    for ip in ${online[@]};
+    do
+        for action in ${entity[@]};
+        do
+            curl http://$ip:8983/solr/$action/dataimport
+        done
+    done
+else
+    for ip in ${offline[@]};
+    do
+        for action in ${entity[@]};
+        do
+            curl http://$ip:8983/solr/$action/dataimport
+        done
+    done
+fi

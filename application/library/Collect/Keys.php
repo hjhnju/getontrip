945361225594877e62c7412b1284f9336c50a369
fanyingying <fanyingying@xingjiaodai.com>
2015-11-06 15:22:52 +0800

    add code

--- application/library/Collect/Keys.php
+++ application/library/Collect/Keys.php
@@ -5,21 +5,21 @@ class Collect_Keys {
 
     const REDIS_SIGHT_INFO_KEY   = 'collect_sight_info';
 
-    const REDIS_ANSWER_INFO_KEY  = 'collect_answer_info';
+    const REDIS_ANSWER_INFO_KEY  = 'collect_city_info';
     
-    const REDIS_THEME_INFO_KEY   = 'collect_theme_info';
+    const REDIS_THEME_INFO_KEY   = 'collect_book_info';
     
     const REDIS_LATE_KEY         = '%s_late_%s';
     
     const REDIS_TOTAL_KEY        = '%s_total';
     
-    const TOPIC     = 1;
+    const TOPIC     = 4;
     
     const SIGHT     = 2;
     
-    const ANSWER    = 3;
+    const CITY    = 3;
      
-    const THEME     = 4;
+    const BOOK    = 5;
 
     public static function getTopicInfoKey(){
         return self::REDIS_TOPIC_INFO_KEY;
@@ -29,12 +29,12 @@ class Collect_Keys {
     	return self::REDIS_SIGHT_INFO_KEY;
     }
 
-    public static function getAnswerInfoKey(){
-    	return self::REDIS_ANSWER_INFO_KEY;
+    public static function getCityInfoKey(){
+    	return self::REDIS_CITY_INFO_KEY;
     }
    
-    public static function getThemeInfoKey(){
-        return self::REDIS_THEME_INFO_KEY;
+    public static function getBookInfoKey(){
+        return self::REDIS_BOOK_INFO_KEY;
     }
     
     public static function getLateKeyName($id,$during){
@@ -51,10 +51,10 @@ class Collect_Keys {
                 return self::REDIS_TOPIC_INFO_KEY;
             case self::SIGHT:
                 return self::REDIS_SIGHT_INFO_KEY;
-            case self::ANSWER:
-                return self::REDIS_ANSWER_INFO_KEY;
-            case self::THEME:
-                return self::REDIS_THEME_INFO_KEY;
+            case self::CITY:
+                return self::REDIS_CITY_INFO_KEY;
+            case self::BOOK:
+                return self::REDIS_BOOK_INFO_KEY;
             default:
                 break;
         }

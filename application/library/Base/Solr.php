2cb8703e0292e0e1cada424a8a2e8df82e25c106
fanyingying <fanyingying@xingjiaodai.com>
2015-11-06 14:31:12 +0800

    add code

--- application/library/Base/Solr.php
+++ application/library/Base/Solr.php
@@ -9,10 +9,7 @@ class Base_Solr {
     public static function  getInstance(){
         $objSolr = Base_Config::getConfig('solr');
         $arrSolr = $objSolr->toArray();
-        foreach ($arrSolr as $val){
-            $arrRet[] = $val;
-        }
-        $key  = array_rand($arrRet);
-        return $arrRet[$key];
+        $key     = array_rand($arrSolr);
+        return $arrSolr[$key];
     }
 }

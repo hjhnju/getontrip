d94f5f37aae99d8f579ba2bb2f8d112eff3cff88
fanyingying <fanyingying@xingjiaodai.com>
2015-12-02 15:04:59 +0800

    话题数据生成训练样本向量

---
+++ script/train/createIndex.php
@@ -0,0 +1,66 @@
+<?php
+require_once "config.php";
+$total_topic           = 0;
+$total_desc            = 0;
+if(!is_dir(WORK_PATH)){
+    mkdir(WORK_PATH);
+}
+
+//创建景点索引文件
+$fp = fopen(WORK_PATH.INDEX_SIGHT, "w");
+$listSightMeta = new Sight_List_Meta();
+$listSightMeta->setPagesize(PHP_INT_MAX);
+$listSightMeta->setOrder('`id` asc');
+$arrSightMeta  = $listSightMeta->toArray();
+foreach ($arrSightMeta['list'] as $index => $val){
+    $sightNames = $val['name'];
+    $objSight   = new Sight_Object_Sight();
+    $objSight->fetch(array('id' => $val['id']));
+    if(!empty($objSight->name)){
+        $sightNames .=",".$objSight->name;
+    }
+    $str = sprintf("%d\t%d\t%s\r\n",$index+1, $val['id'],$sightNames);
+    fputs($fp,$str);
+}
+fclose($fp);
+
+//创建话题索引文件
+$fp = fopen(WORK_PATH.INDEX_TOPIC, "w");
+$listTopic = new Topic_List_Topic();
+$listTopic->setPagesize(PHP_INT_MAX);
+$listTopic->setFilter(array('status' => Topic_Type_Status::PUBLISHED));
+$listTopic->setOrder('`id` asc');
+$arrTopic  = $listTopic->toArray();
+foreach ($arrTopic['list'] as $index => $val){
+    $str = sprintf("%d:%d\r\n",$index + 1, $val['id']);
+    fputs($fp,$str);
+}
+$total_topic = $listTopic->getTotal();
+fclose($fp);
+
+//创建景点描述索引文件
+$fp = fopen(WORK_PATH.INDEX_SIGHT_DESC, "w");
+$listSightMeta = new Sight_List_Meta();
+$listSightMeta->setPagesize(PHP_INT_MAX);
+$listSightMeta->setOrder('`id` asc');
+$arrSightMeta  = $listSightMeta->toArray();
+foreach ($arrSightMeta['list'] as $index => $val){
+    if(!empty($val['describe'])){
+        $total_desc += 1;
+        $str = sprintf("%d:%d\r\n",$total_topic + $index + 1, $val['id']);
+        fputs($fp,$str);
+    }
+}
+fclose($fp);
+
+//创建景点百科索引文件
+/*$fp = fopen(WORK_PATH.INDEX_SIGHT_WIKI, "w");
+$listSightMeta = new Sight_List_Meta();
+$listSightMeta->setPagesize(PHP_INT_MAX);
+$listSightMeta->setOrder('`id` asc');
+$arrSightMeta  = $listSightMeta->toArray();
+foreach ($arrSightMeta['list'] as $index => $val){
+    $str = sprintf("%d:%d\r\n",$total_topic + $total_desc + $index + 1, $val['id']);
+    fputs($fp,$str);
+}
+fclose($fp);*/
---
+++ script/train/getInputVector.php
@@ -0,0 +1,76 @@
+<?php
+require_once "config.php";
+$arrSight   = file(WORK_PATH.INDEX_SIGHT);
+$fp         = fopen(WORK_PATH.INPUT_VECTOR,"w");
+$id         = '';
+$sightId    = '';
+$sightNames = '';
+$listTopic = new Topic_List_Topic();
+echo $listTopic->getTotal();;die;
+foreach ($arrSight as $sight){
+    sscanf($sight,"%s\t%s\t%[^\r]",$id,$sightId,$sightNames);
+    
+    //对于每个景点，依次取出其话题，描述，百科的内容生成向量
+    $listSightTopic = new Sight_List_Topic();
+    $listSightTopic->setFilter(array('sight_id' => $sightId));
+    $listSightTopic->setPagesize(PHP_INT_MAX);
+    $arrSightTopic  = $listSightTopic->toArray();
+    foreach ($arrSightTopic['list'] as $val){
+        $objTopic   = new Topic_Object_Topic();
+        $objTopic->fetch(array('id' => $val['topic_id']));
+        $str        = '';
+        foreach ($arrSight as $temp){
+            $tmpid         = '';
+            $tmpsightId    = '';
+            $tmpsightNames = '';
+            sscanf($temp,"%s\t%s\t%[^\r]",$tmpid,$tmpsightId,$tmpsightNames);
+            $arrTemp = explode(",",$tmpsightNames);
+            foreach ($arrTemp as $data){
+                if(strstr($objTopic->title,$data) !== false){
+                    $str .= $tmpid.":1\t";
+                }
+            }
+        }
+        fwrite($fp,$str."\r\n");
+    }
+    
+    $listSightTag = new Sight_List_Tag();
+    $listSightTag->setFilter(array('sight_id' => $sightId));
+    $listSightTag->setPagesize(PHP_INT_MAX);
+    $arrSightTag  = $listSightTag->toArray();
+    foreach ($arrSightTag['list'] as $val){
+        $listTopicTag = new Topic_List_Tag();
+        $listTopicTag->setFilter(array('tag_id' => $val['tag_id']));
+        $listTopicTag->setPagesize(PHP_INT_MAX);
+        $arrTopicTag  = $listTopicTag->toArray();
+        foreach ($arrTopicTag['list'] as $topictag){
+            $objTopic   = new Topic_Object_Topic();
+            $objTopic->fetch(array('id' => $topictag['topic_id']));
+            $str        = '';
+            foreach ($arrSight as $temp){
+                $tmpid         = '';
+                $tmpsightId    = '';
+                $tmpsightNames = '';
+                sscanf($temp,"%s\t%s\t%[^\r]",$tmpid,$tmpsightId,$tmpsightNames);
+                $arrTemp = explode(",",$tmpsightNames);
+                foreach ($arrTemp as $data){
+                    if(strstr($objTopic->title,$data) !== false){
+                        $str .= $tmpid.":1\t";
+                    }
+                }
+            }
+            fwrite($fp,$str."\r\n");
+        }
+    }
+    
+    //描述对应的向量
+    $objSightMeta = new Sight_Object_Meta();
+    $objSightMeta->fetch(array('id' => $sightId));
+    if(!empty($objSightMeta->describe)){
+        $str = $id.":1\r\n";
+        fwrite($fp, $str);
+    }    
+    
+    //百科对应的向量
+}
+fclose($fp);

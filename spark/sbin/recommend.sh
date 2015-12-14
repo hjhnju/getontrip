650578cdfa06cfd9cebdd09a0d76c5ab4d5fcf8d
hjhnju <hjh.nju@gmail.com>
2015-12-14 14:20:38 +0800

    使用tfidf的推荐实现

--- spark/sbin/recommend.sh
+++ spark/sbin/recommend.sh
@@ -1,11 +1,11 @@
 #/bin/bash
-rm -rf data/profiles
-rm -rf data/labelrdds
+rm -rf data/similarity.out
 spark-submit \
-  --class "recommend.ContentBasedProfiling" \
+  --class "recommend.ContentBasedRecommend" \
   --master local[4] \
   --executor-memory 8G \
   --driver-memory 8G \
   --conf spark.shuffle.spill=false \
   --conf "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   target/scala-2.10/getontrip-sparking_2.10-1.0.jar
+
--- spark/src/main/scala/recommend/ContentBasedProfiling.scala
+++ spark/src/main/scala/recommend/ContentBasedProfiling.scala
@@ -1,10 +1,15 @@
 package recommend
 
+import java.io.{ObjectOutputStream, FileOutputStream, DataOutputStream}
+
+import breeze.linalg.DenseVector
 import org.apache.spark.mllib.feature.{IDF, HashingTF}
+import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
-import org.apache.spark.mllib.linalg.Vector
+import org.apache.spark.mllib.linalg.{Vectors, SparseVector, Vector}
 
 /**
   *  基于内容的推荐, 学习景点的喜好特征
@@ -17,54 +22,87 @@ object ContentBasedProfiling {
         val conf = new SparkConf().setAppName("GetOntrip Sparking")
         val sc   = new SparkContext(conf)
 
+        //1. item representation (tf-idf vector)
         var labelMap = Map[String, String]()
-        val profileOutput = scala.tools.nsc.io.File("data/profiles")
-
+        //documents: label, item_list
+        var documents: RDD[(String, Seq[String])] = null
         for(row <- scala.io.Source.fromFile("data/labels.top5").getLines.map{line => line.split("""\s+""")}) {
             if (row.length >= 2) {
-                labelMap += (row(0) -> row(1))
-                val label = row(0)
-
-                //1. item representation (tf-idf vector)
-                //input: documents with words
-                //output: RDD[Vector]
-                val documents: RDD[Seq[String]] = sc.textFile("data/documents/" + label).map(_.split("""\s+""").toSeq)
-                val tfidf = presentItem(sc, documents)
-
-                //2. profile learning (这里采用mean of vectors, 广义的可以是分类模型)
-                //input: RDD[Vector]
-                //output: Vector(using) or Model
-                val profile: Vector = profileLearning(tfidf)
-                profileOutput.appendAll(profile.toArray.mkString)
-                //3. recommend
-                // do recommend in ContentBasedRecommend
+                labelMap   += (row(0) -> row(1))
+                val label   = row(0)
+                val newDocs = sc.textFile("data/documents/" + label).map(
+                    line => (label, line.split("""\s+""").drop(1).toSeq)
+                )
+                if(documents == null) {
+                    documents = newDocs
+                } else {
+                    documents = documents.union(newDocs)
+                }
             }
         }
+
+        // vector space model
+        val vsmDocuments = presentItem(sc, documents)
+        vsmDocuments.cache()
+
+        //2. profile learning (这里采用mean of vectors, 广义的可以是分类模型)
+        val profiles = profileLearning(sc, vsmDocuments)
+        MLUtils.saveAsLibSVMFile(profiles, "data/profiles.libsvm")
+
+        //3. recommend
+        // do recommend in ContentBasedRecommend
     }
 
     /**
-      * //1. item representation (tf-idf vector)
-      * //input: documents with words
-      * //output: RDD[Vector]
+      * represent documents with VSM(Vector Space Model)
       * @param sc
+      * @param documents
       * @return
       */
-    def presentItem(sc: SparkContext, documents: RDD[Seq[String]]): RDD[Vector] = {
-        val hashingTF = new HashingTF()
-        val tf: RDD[Vector] = hashingTF.transform(documents)
+    def presentItem(sc: SparkContext, documents: RDD[(String, Seq[String])]): RDD[(String, Vector)] = {
+
+        val hashingTF = new HashingTF(1048356)
+        val tf: RDD[Vector] = hashingTF.transform(documents.values).map(v => v.toSparse)
         tf.cache()
         // 最少在两篇文章出现的才计入idf, 否则idf=0
-        val idf = new IDF(minDocFreq = 2).fit(tf)
-        val tfidf: RDD[Vector] = idf.transform(tf)
-        return tfidf
+        val idfModel = new IDF(minDocFreq = 2).fit(tf)
+        // 保存idf model, 推荐时使用
+        val oos = new ObjectOutputStream(new FileOutputStream("data/idf.model"))
+        oos.writeObject(idfModel)
+        oos.close
+
+        val tfidf: RDD[Vector] = idfModel.transform(tf).map(v => v.toSparse)
+
+        // 合并回去
+        val vectors: RDD[(Long, Vector)]  = tfidf.zipWithIndex().map(line => (line._2, line._1))
+        val labels: RDD[(Long, String)]   = documents.keys.zipWithIndex().map(line => (line._2, line._1))
+        val docVsm: RDD[(String, Vector)] = labels.join(vectors).values
+
+        return docVsm
     }
 
-    def profileLearning(tfidf: RDD[Vector]): Vector = {
-        //val profileVector = new Vector()
-        tfidf.cache()
-        val summary: MultivariateStatisticalSummary = Statistics.colStats(tfidf)
-        val profileVector: Vector = summary.mean
-        return profileVector
+    /**
+      * "偏好特征"的学习, 这里采用一个类别中所有向量的平均值作为该类的偏好特征
+      * @param sc
+      * @param docVsm
+      * @return
+      */
+    def profileLearning(sc: SparkContext, docVsm: RDD[(String, Vector)]): RDD[LabeledPoint] = {
+
+        // 所有 偏好特征 的集合
+        var profiles = Array[LabeledPoint]()
+
+        //仅适用小数据集labelPoints
+        val byKey    = docVsm.groupByKey().collect()
+        val rddByKey = byKey.map{case (k,v) => k -> sc.makeRDD(v.toSeq)}
+        rddByKey.foreach{
+            case (k, rdd) =>
+                val summary: MultivariateStatisticalSummary = Statistics.colStats(rdd)
+                val profile: Vector = summary.mean.toSparse
+                profiles +:= LabeledPoint(k.toDouble, profile)
+        }
+
+        return sc.makeRDD(profiles)
     }
 
 }

918a833ee1da003abf64383807eb7c880cb05979
hjhnju <hjh.nju@gmail.com>
2015-12-15 16:50:18 +0800

    modify input output dir

--- spark/sbin/recommend.sh
+++ spark/sbin/recommend.sh
@@ -1,11 +1,19 @@
 #/bin/bash
+cd `dirname $0`/../
+echo "executing path = "`pwd`
+cp data/newdocs.20151211 data/newdocs.txt
 rm -rf data/similarity.out
 spark-submit \
   --class "recommend.ContentBasedRecommend" \
   --master local[4] \
-  --executor-memory 8G \
-  --driver-memory 8G \
+  --executor-memory 3G \
+  --driver-memory 3G \
   --conf spark.shuffle.spill=false \
   --conf "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   target/scala-2.10/getontrip-sparking_2.10-1.0.jar
+
+cp data/similarity.out data/similarity.20151211
+
+
+
 

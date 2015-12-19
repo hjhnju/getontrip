b4aba7ebc8c9ac5cd9bf3ef4e389baaa698e580a
hjhnju <hjh.nju@gmail.com>
2015-12-19 11:34:05 +0800

    完善推荐脚本传参

--- spark/sbin/profiling.sh
+++ spark/sbin/profiling.sh
@@ -6,8 +6,8 @@ rm -rf data/labelrdds
 spark-submit \
   --class "recommend.ContentBasedProfiling" \
   --master local[4] \
-  --executor-memory 8G \
-  --driver-memory 8G \
+  --executor-memory 3G \
+  --driver-memory 3G \
   --conf spark.shuffle.spill=false \
   --conf "spark.executor.extraJavaOptions=-XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   target/scala-2.10/getontrip-sparking_2.10-1.0.jar

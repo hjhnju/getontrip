74c8b8627a19e59d3d7cfd8fab79c94e0640e393
huwei <whu404@163.com>
2016-01-19 11:12:40 +0800

    add code

--- application/controllers/Audio.php
+++ application/controllers/Audio.php
@@ -21,15 +21,85 @@ class AudioController extends Base_Controller_Page {
             header("HTTP/1.1 404 Not Found");
             exit;
         }
+        if(strstr($_SERVER['HTTP_USER_AGENT'],"Chrome")){
+            $this->chromeownload($hash);
+        }else{
+            $this->download($hash);
+        }
+        exit;
+    }
+    
+    
+    /** 下载
+     * @param String  $file   要下载的文件路径
+     * @param String  $name   文件名称,为空则与下载的文件名称一样
+     * @param boolean $reload 是否开启断点续传
+     */
+    public function  download($hash){
         $oss   = Oss_Adapter::getInstance();
         $audio = $oss->getContent($hash);
-        if (empty($audio)) {
+        if(!empty($audio)){
+            $file_size = $oss->getMetaLen($hash);
+            $ranges = $this->getRange($file_size);
+            ob_clean();
+            header('cache-control:public');
+            header('content-type:audio/mpeg');
+    
+            if($ranges!=null){ // 使用续传;
+                header('HTTP/1.1 206 Partial Content');
+                header('Accept-Ranges:bytes');
+    
+                // 剩余长度
+                header(sprintf('content-length:%u',$ranges['end']-$ranges['start']));
+    
+                // range信息
+                header(sprintf('content-range:bytes %s-%s/%s', $ranges['start'], $ranges['end'], $file_size));
+    
+                // fp指针跳到断点位置
+                //fseek($fp, sprintf('%u', $ranges['start']));
+                $audio = substr($audio,$ranges['start']);
+            }else{
+                header('Content-length:'.$file_size);
+            }
+            echo $audio; 
+        }else{
             header("HTTP/1.1 404 Not Found");
-            exit;
         }
+    }
+    
+    public function chromeownload($hash){
+        $oss   = Oss_Adapter::getInstance();
+        $audio = $oss->getContent($hash);
+        $file_size = $oss->getMetaLen($hash);
         ob_clean();
-        header('Content-type: audio/mpeg') ;
+        header('content-type:audio/mpeg');
+        header('Content-length:'.$file_size);
         echo $audio;
-        exit;
+    }
+    
+    
+    
+    /** 获取header range信息
+     * @param  int   $file_size 文件大小
+     * @return Array
+     */
+    private function getRange($file_size){
+        if(isset($_SERVER['HTTP_RANGE']) && !empty($_SERVER['HTTP_RANGE'])){
+            $range = $_SERVER['HTTP_RANGE'];
+            $range = preg_replace('/[\s|,].*/', '', $range);
+            $range = explode('-', substr($range, 6));
+            if(count($range)<2){
+                $range[1] = $file_size;
+            }
+            $range = array_combine(array('start','end'), $range);
+            if(empty($range['start'])){
+                $range['start'] = 0;
+            }
+            if(empty($range['end'])){
+                $range['end'] = $file_size;
+            }
+            return $range;
+        }
+        return null;
     }
 }

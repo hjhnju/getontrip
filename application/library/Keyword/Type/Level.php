c2d376554c4dc6ab23c510077c4a0ed77cb52585
huwei <whu404@163.com>
2016-01-20 17:56:21 +0800

    add code

---
+++ application/library/Keyword/Type/Level.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * 词条层级类型
+ * @author huwei
+ *
+ */
+class Keyword_Type_Level extends Base_Type {
+    /**
+     * 1 城市级,sight_id为城市id
+     * @var integer
+     */
+    const CITY = 1;  
+
+    /**
+     * 2 景点级,sight_id为景点id
+     * @var integer
+     */
+    const SIGHT        = 2;
+        
+    /**
+     * 3 景观级,二级景观,sight_id为景观id
+     * @var integer
+     */
+    const LANDSCAPE    = 3;
+    
+    /**
+     * 状态名
+     * @var array
+     */
+    public static $names = array(
+        self::CITY         => '城市级',
+        self::SIGHT        => '景点级',
+        self::LANDSCAPE    => '景观级',
+    );
+}
